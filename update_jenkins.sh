56ce0a6bad72f6f1243c4b7aba96de9865e8c5a4
Matias Palacios <matiasepalacios@users.noreply.github.com>
2020-03-19 16:53:59 -0300

    Update update_jenkins.sh

--- update_jenkins.sh
+++ update_jenkins.sh
@@ -15,25 +15,25 @@ __root="$(cd "$(dirname "${__dir}")" && pwd)"
 
 __jenkins_dir="/lib/jenkins/" # this is where jenkins lives
 
-echo 'Script started at' $(date +"%D at %r") '\n'
+echo 'Script started at' $(date +"%D at %r")
 
-echo 'Stopping Jenkins...\n'
+echo 'Stopping Jenkins...'
 service jenkins stop
 
-echo 'Deleting old backed up jenkins file...\n'
+echo 'Deleting old backed up jenkins file...'
 rm -f ${__jenkins_dir}jenkins.war.old
 
-echo 'Backing up current Jenkins file...\n'
+echo 'Backing up current Jenkins file...'
 mv ${__jenkins_dir}jenkins.war ${__jenkins_dir}jenkins.war.old
 
-echo 'Deleting current Jenkins file...\n'
+echo 'Deleting current Jenkins file...'
 rm -rf ${__jenkins_dir}jenkins.war
 
-echo 'Getting latest Jenkins file from http://mirrors.jenkins-ci.org/war/latest/jenkins.war...\n'
+echo 'Getting latest Jenkins file from http://mirrors.jenkins-ci.org/war/latest/jenkins.war...'
 wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war -P ${__jenkins_dir}
 
-echo 'Starting Jenkins...\n'
+echo 'Starting Jenkins...'
 service jenkins start
 
 
-echo 'Script ended at' $(date +"%D at %r") '\n'
+echo 'Script ended at' $(date +"%D at %r")
